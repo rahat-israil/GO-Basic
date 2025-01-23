2d6ddc33c27b1c60bb8ac9a3636e2ac00603573a
rahat-israil <rahat.bin.israil@gmail.com>
2025-01-23 22:57:54 +0600

    Variables, Datatype & Function

---
+++ main.go
@@ -0,0 +1,28 @@
+package main
+
+import "fmt"
+
+
+// func main() {
+// 	fmt.Println("Hello, World!");
+// }
+
+
+// Variables & Data Types
+// func main() {
+//     var name = "Rahat";
+//     age := 24;
+//     var x int = 5;
+
+//     fmt.Println(name, age, x);
+// }
+
+//  Function
+func main(){
+    a := 5;
+    b := 6;
+
+    sum := a * b;
+
+    fmt.Println(sum);
+}
