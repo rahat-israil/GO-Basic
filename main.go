befff17f1c141941debe9a2a7c2ae3c0137b37d3
rahat-israil <rahat.bin.israil@gmail.com>
2025-01-24 21:43:42 +0600

    if else

--- main.go
+++ main.go
@@ -18,11 +18,35 @@ import "fmt"
 // }
 
 //  Function
-func main(){
+func main1(){
     a := 5;
     b := 6;
 
     sum := a * b;
 
     fmt.Println(sum);
+}
+
+
+// if else
+func main2() {
+    x := 5;
+
+    if (x > 10) {
+        fmt.Println("x is greater than 10");
+    } else {
+        fmt.Println("x is less than 10");
+    }
+}
+
+func main() {
+    y := 10;
+
+    if (y > 10 && y < 20) {
+        fmt.Println("y is greater than 10");
+    } else if (y > 9 || y == 20) {
+        fmt.Println("Special case")
+    } else {
+        fmt.Println("Nothing");
+    }
 }
